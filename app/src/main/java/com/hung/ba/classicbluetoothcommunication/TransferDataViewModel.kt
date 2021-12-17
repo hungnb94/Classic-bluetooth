8fce4d27d9fec521435aa487e29bebac521dc056
Nguyen Ba Hung <hung@bio-rithm.com>
2021-12-17 14:07:18 +0700

    Transfer data by packet length and connection interval

--- app/src/main/java/com/hung/ba/classicbluetoothcommunication/TransferDataViewModel.kt
+++ app/src/main/java/com/hung/ba/classicbluetoothcommunication/TransferDataViewModel.kt
@@ -23,6 +23,9 @@ class TransferDataViewModel : ViewModel() {
     private val _transferDuration = MutableLiveData(0L)
     val transferDuration: LiveData<Long> = _transferDuration
 
+    private var packetLength = 1
+    private var connectionInterval = 0
+
     private lateinit var bytes: List<Byte>
     private val bluetoothService: BluetoothChatService? = BluetoothChat.mChatService
 
@@ -40,46 +43,34 @@ class TransferDataViewModel : ViewModel() {
         }
     }
 
+    fun onPacketLengthChange(text: CharSequence?, start: Int, before: Int, count: Int) {
+        packetLength = Integer.parseInt(text?.toString() ?: "0")
+    }
+
+    fun onConnectionIntervalChange(text: CharSequence?, start: Int, before: Int, count: Int) {
+        connectionInterval = Integer.parseInt(text?.toString() ?: "0")
+    }
+
     fun startTransfer() {
         viewModelScope.launch(Dispatchers.IO) {
-            transferBytesEverySecond()
+            transferBytes(packetLength, connectionInterval.toLong())
         }
     }
 
-    private fun transferSingleByteWithoutDelay() {
+    private suspend fun transferBytes(packetLength: Int, connectionInterval: Long) {
         val startTime = System.currentTimeMillis()
         Log.i(TAG, "startTransfer: $startTime")
-        var count = 0
         val length = bytes.size.toFloat()
-        bytes.forEach {
-            bluetoothService?.write(byteArrayOf(it))
-            ++count
-            val percent = count / length
-            _percentTransfer.postValue(percent)
-            _transferDuration.postValue(System.currentTimeMillis() - startTime)
-            Log.i(TAG, "startTransfer: percent upload $percent")
-        }
-        val finishTime = System.currentTimeMillis()
-        Log.i(TAG, "finishTransfer: $finishTime, total time: ${finishTime - startTime}")
-    }
-
-    private suspend fun transferBytesEverySecond() {
-        val startTime = System.currentTimeMillis()
-        Log.i(TAG, "startTransfer: $startTime")
-        var count: Int
-        val length = bytes.size.toFloat()
-        val duration = 5 * 60
-        val speed = length / duration
         var position = 0
-        for (i in 1..duration) {
-            delay(1000)
+        while (position < length) {
+            delay(connectionInterval)
             val start = position
-            val end = (speed + start).toInt()
-            count = end
+            val end = start + packetLength
             position = end
+            if (position > length) position = length.toInt()
             val array = bytes.subList(start, end).toByteArray()
             bluetoothService?.write(array)
-            val percent = count / length
+            val percent = position / length
             _percentTransfer.postValue(percent)
             _transferDuration.postValue(System.currentTimeMillis() - startTime)
             Log.i(TAG, "startTransfer: percent upload $percent")
